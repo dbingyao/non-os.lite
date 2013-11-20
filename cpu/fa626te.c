7115e7083627ff5ab363d9bcc4cf11c2b4a1f46b
mike <mikeyeh@faraday-tech.com>
2013-11-20 15:11:02 +0800

    Add more cpu specific func for fa626
    The following functions are added:
    fa626_check_dcache_en
    fa626_repo_cntr
    fa626_set_cntr
    fa626_invalid_dcache_all
    fa626_invalid_icache_all
    fa626_clean_dcache_all
    fa626_clean_invalid_dcache_all
    fa626_invalid_idcache_all
    fa626_invalid_tlb_all
    fa626_drain_wr_buffer
    fa626_invalid_iscratchpad
    fa626_invalid_btb

--- cpu/fa626te.c
+++ cpu/fa626te.c
@@ -1,6 +1,99 @@
 
 extern unsigned int *_pg_tb;
 
+int fa626_repo_cntr() {
+	int val;
+
+	asm volatile("mrc p15, 0, %0, c1, c0, 0"
+		     : "=r" (val): : "memory");
+
+	return val;
+}
+
+void fa626_set_cntr(int val) {
+
+	asm volatile("mcr p15, 0, %0, c1, c0, 0\n\t"
+				"nop \n\t"
+				"nop \n\t"
+				"nop \n\t"
+		     : : "r" (val) : "memory");
+
+}
+
+int fa626_check_dcache_en() {
+	int val;
+
+	asm volatile("mrc p15, 0, %0, c1, c0, 0"
+		     : "=r" (val): : "memory");
+
+	if(val & (1 << 4))
+		return 1;
+	else
+		return 0;
+}
+
+void fa626_invalid_dcache_all() {
+
+	asm volatile("mcr p15, 0, %0, c7, c6, 0"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_invalid_icache_all() {
+
+	asm volatile("mcr p15, 0, %0, c7, c5, 0"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_clean_dcache_all() {
+
+	asm volatile("mcr p15, 0, %0, c7, c10, 0"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_clean_invalid_dcache_all() {
+
+	asm volatile("mcr p15, 0, %0, c7, c14, 0"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_invalid_idcache_all() {
+
+	asm volatile("mcr p15, 0, %0, c7, c7, 0"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_invalid_tlb_all() {
+
+	asm volatile("mcr p15, 0, %0, c8, c7, 0"
+		     : : "r" (0) : "memory");
+
+}
+
+void fa626_drain_wr_buffer() {
+
+	asm volatile("mcr p15, 0, %0, c7, c10, 4\t\n"
+				"nop \n\t"
+				"nop \n\t"
+				"nop \n\t"
+				"nop \n\t"
+				"nop \n\t"
+				"nop \n\t"
+				"nop \n\t"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_invalid_iscratchpad() {
+
+	asm volatile("mcr p15, 0, %0, c7, c5, 5"
+		     : : "r" (0) : "memory");
+}
+
+void fa626_invalid_btb() {
+
+	asm volatile("mcr p15, 0, %0, c7, c5, 6"
+		     : : "r" (0) : "memory");
+}
+
 int enable_d_cache(int argc, char * const argv[]) {
 	int i, val;
 	unsigned int *ttb_addr;
@@ -18,23 +111,16 @@ int enable_d_cache(int argc, char * const argv[]) {
 		     : : "r" (~0));
 
 	/* Read back and then Enable cache */
-	asm volatile("mrc p15, 0, %0, c1, c0, 0"
-		     : "=r" (val): : "memory");
-
+	val = fa626_repo_cntr();
 	val |= ((1 << 12) | (1 << 2) | (1 << 0));
-	asm volatile("mcr p15, 0, %0, c1, c0, 0\n\t"
-				"nop \n\t"
-				"nop \n\t"
-				"nop \n\t"
-		     : : "r" (val) : "memory");
+
+	fa626_set_cntr(val);
 
 	/* Invalid TLB */
-	asm volatile("mcr p15, 0, %0, c8, c7, 0"
-		     : : "r" (0) : "memory");
+	fa626_invalid_tlb_all();
 
 	/* Invalid Dcache, Icache */
-	asm volatile("mcr p15, 0, %0, c7, c7, 0"
-		     : : "r" (0));
+	fa626_invalid_idcache_all();
 
 	return 0;
 }
@@ -44,36 +130,26 @@ int disable_d_cache(int argc, char * const argv[]) {
 	int val;
 
 	/* Clean & Invalidate D-cache */
-	asm volatile("mcr p15, 0, %0, c7, c14, 0"
-		     : : "r" (0) : "memory");
+	fa626_clean_invalid_dcache_all();
 
 	/* Invalidate I-cache */
-	asm volatile("mcr p15, 0, %0, c7, c5, 0"
-		     : : "r" (0) : "memory");
+	fa626_invalid_icache_all();
 
 	/* Drain write buffer(SYNC) */
-	asm volatile("mcr p15, 0, %0, c7, c10, 4"
-		     : : "r" (0) : "memory");
+	fa626_drain_wr_buffer();
 
 	/* Invalidate IScratchpad */
-	asm volatile("mcr p15, 0, %0, c7, c5, 5"
-		     : : "r" (0) : "memory");
+	fa626_invalid_iscratchpad();
 
 	/* Invalidate BTB */
-	asm volatile("mcr p15, 0, %0, c7, c5, 6"
-		     : : "r" (0) : "memory");
+	fa626_invalid_btb();
 
 	/* Read back and then Disable cache */
-	asm volatile("mrc p15, 0, %0, c1, c0, 0"
-		     : "=r" (val): : "memory");
+	val = fa626_repo_cntr();
+	val &= ~((1 << 12) | (1 << 2) | (1 << 0));
 
 	/* Disable I-cache, D-cache, and MMU */
-	val &= ~((1 << 12) | (1 << 2) | (1 << 0));
-	asm volatile("mcr p15, 0, %0, c1, c0, 0\n\t"
-				"nop \n\t"
-				"nop \n\t"
-				"nop \n\t"
-		     : : "r" (val) : "memory");
+	fa626_set_cntr(val);
 
 	return 0;
 }
